a047fbc80132f4ff2010d38af14cd64e86142d17
Seung-9 <ssg9505fj22@naver.com>
2025-01-16 02:04:44 +0900

    init: 스프링 부트 초기 세팅

---
+++ backend/src/main/java/baekya/backend/BaekyaApplication.java
@@ -0,0 +1,13 @@
+package baekya.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaekyaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/baekya/backend/BaekyaApplicationTests.java
@@ -0,0 +1,13 @@
+package baekya.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
